791e6c77924c17929950b46d9bb5814e25e0e9e2
David Ilas <davidilas1@gmail.com>
2022-09-08 13:04:36 +0200

    fix after review 3

--- src/Entity/Embeddable/NotificationSettings.php
+++ src/Entity/Embeddable/NotificationSettings.php
@@ -22,5 +22,62 @@ class NotificationSettings
     #[ORM\Column(type: Types::BOOLEAN)]
     private bool $emailVerified = false;
 
+    #[ORM\Column(type: Types::BOOLEAN)]
     private bool $push = false;
+
+    public function getTelegramId(): string
+    {
+        return $this->telegramId;
+    }
+
+    public function setTelegramId(string $telegramId): NotificationSettings
+    {
+        $this->telegramId = $telegramId;
+        return $this;
+    }
+
+    public function isTelegramVerified(): bool
+    {
+        return $this->telegramVerified;
+    }
+
+    public function setTelegramVerified(bool $telegramVerified): NotificationSettings
+    {
+        $this->telegramVerified = $telegramVerified;
+        return $this;
+    }
+
+
+    public function getEmailId(): string
+    {
+        return $this->emailId;
+    }
+
+    public function setEmailId(string $emailId): NotificationSettings
+    {
+        $this->emailId = $emailId;
+        return $this;
+    }
+
+    public function isEmailVerified(): bool
+    {
+        return $this->emailVerified;
+    }
+
+    public function setEmailVerified(bool $emailVerified): NotificationSettings
+    {
+        $this->emailVerified = $emailVerified;
+        return $this;
+    }
+
+    public function isPush(): bool
+    {
+        return $this->push;
+    }
+
+    public function setPush(bool $push): NotificationSettings
+    {
+        $this->push = $push;
+        return $this;
+    }
 }
