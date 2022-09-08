a1529498b240cb7e016b67c6e8fa5f49d9202a58
michal <mich.gurcik@gmail.com>
2022-09-08 09:51:04 +0200

    Registration refactoring update

--- src/Dto/User/RegistrationInput.php
+++ src/Dto/User/RegistrationInput.php
@@ -2,7 +2,6 @@
 
 namespace App\Dto\User;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class RegistrationInput
@@ -15,26 +14,4 @@ class RegistrationInput
         minMessage: 'Password must be at least 6 long'
     )]
     public string $password;
-
-//    public string $roles;
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-        return $this;
-    }
-
-    public function setPlainPassword(string $plainPassword): self
-    {
-        $this->password = $plainPassword;
-        return $this;
-    }
-
-    public static function fromRequest(Request $request): self
-    {
-        $input = $request->toArray();
-        return (new self())
-            ->setEmail($input['email'] ?? null)
-            ->setPlainPassword($input['password'] ?? null);
-    }
 }
