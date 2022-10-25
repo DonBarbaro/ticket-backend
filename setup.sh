15261fcad5a0de24c92c5858e3e61f9b13ce7854
Dávid Iľaš <davidilas1@gmail.com>
2022-10-25 11:26:19 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ docker-compose exec php php bin/console cache:clear --no-warmup
 docker-compose exec php php bin/console lexik:jwt:generate-keypair --skip-if-exists
 
 # recreate copytrading database
-docker-compose exec php php api/bin/console doctrine:database:drop --if-exists --force
-docker-compose exec php php api/bin/console doctrine:database:create
-docker-compose exec php php api/bin/console doctrine:migration:migrate --no-interaction --allow-no-migration
-docker-compose exec php php api/bin/console doctrine:fixture:load
+docker-compose exec php php bin/console doctrine:database:drop --if-exists --force
+docker-compose exec php php bin/console doctrine:database:create
+docker-compose exec php php bin/console doctrine:migration:migrate --no-interaction --allow-no-migration
+docker-compose exec php php bin/console doctrine:fixture:load
